5a2369e6ead445ccf976f507fab1c199d98f2757
ChiLigionCEO5 <55261802+ChiLigionCEO5@users.noreply.github.com>
2019-11-07 16:26:16 -0600

    Created a text. We also created a touch bug for the ball to reach it goal.

---
+++ Beer-game-master/Beer-game-master/Unity Project/Assets/Scripts/MovingRamp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingRamp : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody;
+    private float verrticalInput;
+    public string inputAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+        
+    }
+    private void Update()
+    {
+        verrticalInput = Input.GetAxis(inputAxis);
+    }
+    // Use fixed update for physics dode, because we need to be
+    // careful about how often we call expensive, hardware intensive
+    // physics stuff.
+
+    private void FixedUpdate()
+    {
+        
+        rigidbody.velocity = new Vector2(0, verrticalInput* moveSpeed);
+    }
+}
